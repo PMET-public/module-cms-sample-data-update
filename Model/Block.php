08d0c80b92e196cfb75ed2f90e61e7d18f289830
Greg Mills <gmills@magento.com>
2016-08-03 12:51:51 -0400

    add data install

---
+++ Model/Block.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace MagentoEse\CmsSampleDataUpdate\Model;
+
+use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
+
+/**
+ * Class Block
+ */
+class Block
+{
+    /**
+     * @var \Magento\Framework\Setup\SampleData\FixtureManager
+     */
+    private $fixtureManager;
+
+    /**
+     * @var \Magento\Framework\File\Csv
+     */
+    protected $csvReader;
+
+    /**
+     * @var \Magento\Cms\Model\BlockFactory
+     */
+    protected $blockFactory;
+
+    /**
+     * @var Block\Converter
+     */
+    protected $converter;
+
+    /**
+     * @param SampleDataContext $sampleDataContext
+     * @param \Magento\Cms\Model\BlockFactory $blockFactory
+     * @param Block\Converter $converter
+     */
+    public function __construct(
+        SampleDataContext $sampleDataContext,
+        \Magento\Cms\Model\BlockFactory $blockFactory,
+        Block\Converter $converter,
+        \Magento\Store\Model\Store $storeView
+    ) {
+        $this->fixtureManager = $sampleDataContext->getFixtureManager();
+        $this->csvReader = $sampleDataContext->getCsvReader();
+        $this->blockFactory = $blockFactory;
+        $this->converter = $converter;
+        $this->config = require 'Config.php';
+        $this->storeView = $storeView;
+    }
+
+    public function install(array $fixtures)
+    {
+        foreach ($fixtures as $fileName) {
+            $fileName = $this->fixtureManager->getFixture($fileName);
+            if (!file_exists($fileName)) {
+                continue;
+            }
+
+            $rows = $this->csvReader->getData($fileName);
+            $header = array_shift($rows);
+
+            foreach ($rows as $row) {
+                $data = [];
+                foreach ($row as $key => $value) {
+                    $data[$header[$key]] = $value;
+                }
+                $row = $data;
+                $data = $this->converter->convertRow($row);
+                $cmsBlock = $this->saveCmsBlock($data['block']);
+                $cmsBlock->unsetData();
+            }
+        }
+    }
+
+    /**
+     * @param array $data
+     * @return \Magento\Cms\Model\Block
+     */
+    protected function saveCmsBlock($data)
+    {
+        $cmsBlock = $this->blockFactory->create();
+        $cmsBlock->getResource()->load($cmsBlock, $data['identifier']);
+
+        //get view id from view code
+        $_viewId = $this->storeView->load($this->config['viewCode'])->getStoreId();
+
+        if (!$cmsBlock->getData()) {
+            $cmsBlock->setData($data);
+        } else {
+            $cmsBlock->addData($data);
+        }
+        $cmsBlock->setStoreId($_viewId);
+        $cmsBlock->setIsActive(1);
+        $cmsBlock->save();
+        return $cmsBlock;
+    }
+}
